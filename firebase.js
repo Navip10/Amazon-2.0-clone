0a394a51a31191218de3f1f97b061e875f342cf5
Navier Polanco <npolanco0100@hotmail.com>
2022-07-07 20:47:13 -0400

    added add to cart buttons and firebase config

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBm49wIKmGKjdDhxHOBdJl6DmXyUSSOkpk",
+  authDomain: "clone-two-db52e.firebaseapp.com",
+  projectId: "clone-two-db52e",
+  storageBucket: "clone-two-db52e.appspot.com",
+  messagingSenderId: "636529549733",
+  appId: "1:636529549733:web:f42e33a80e20770e0f884c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
